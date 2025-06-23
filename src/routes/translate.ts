906daac300d5d43597c947ab957b922af33c1c98
erfilho <erineldx@gmail.com>
2025-06-23 13:48:21 -0300

    fix(translate-route): handle request body parsing and validation

--- src/routes/translate.ts
+++ src/routes/translate.ts
@@ -1,23 +1,23 @@
-import express from "express";
+import express from 'express';
 
-import { openai } from "../config/openai";
+import { openai } from '../config/openai';
 
 const router = express.Router();
 
-router.post("/", async (req, res) => {
-  const text = req.body;
+router.post('/', async (req, res) => {
+  const { text } = req.body;
 
   try {
     const response = await openai.chat.completions.create({
-      model: "gpt-4o-mini",
+      model: 'gpt-4o-mini',
       messages: [
         {
-          role: "system",
+          role: 'system',
           content:
-            "Você é um tradutor de português para Libras. A sua resposta será usada no vLibras para exibir em tempo real, retorne somente as palavras traduzidas, sem explicações ou contexto adicional.",
+            'Você é um tradutor de português para Libras. A sua resposta será usada no vLibras para exibir em tempo real, retorne somente as palavras traduzidas, sem explicações ou contexto adicional.',
         },
         {
-          role: "user",
+          role: 'user',
           content: `Traduza de português para libras, considerando a simplificação de sinais, a seguinte frase: '${text}' e me retorne somente a sequência de palavras, sem ordem, pontuação, significados ou explicações adicionais.`,
         },
       ],
@@ -25,8 +25,8 @@ router.post("/", async (req, res) => {
 
     res.json({ translation: response.choices[0].message.content });
   } catch (err) {
-    console.error("Error on Libras translation! ", err);
-    res.status(500).json({ error: "Error on translation!" });
+    console.error('Error on Libras translation! ', err);
+    res.status(500).json({ error: 'Error on translation!' });
   }
 });
 
