ca7279c7460a495d1f83d3422bc990dee0c3ec51
Erineldo Filho <erineldot@gmail.com>
2025-07-06 21:04:18 -0300

    fix: error with chunk transcription

--- src/routes/transcribe.ts
+++ src/routes/transcribe.ts
@@ -1,16 +1,15 @@
-import express from "express";
-import multer from "multer";
+import express from 'express';
+import multer from 'multer';
 
-import { openai } from "../config/openai";
+import { openai } from '../config/openai';
 
-import { convertToMp3 } from "../utils/audioConverter";
-import { changeFileExtension, deleteFile } from "../utils/fileManager";
-import fs from "fs";
+import { deleteFile } from '../utils/fileManager';
+import fs from 'fs';
 
 const router = express.Router();
 
 const storage = multer.diskStorage({
-  destination: "./tmp",
+  destination: './tmp',
   filename: (req, file, cb) => {
     cb(null, `${Date.now()}-${file.originalname}`);
   },
@@ -18,30 +17,26 @@ const storage = multer.diskStorage({
 
 const upload = multer({ storage });
 
-router.post("/", upload.single("audio"), async (req, res) => {
+router.post('/', upload.single('audio'), async (req, res) => {
   if (!req.file) {
-    res.status(400).json({ error: "No file was sent!" });
+    res.status(400).json({ error: 'No file was sent!' });
     return;
   }
 
-  const inputPath = req.file.path;
-  const outputPath = changeFileExtension(inputPath, ".mp3");
+  const filePath = req.file.path;
 
   try {
-    await convertToMp3(inputPath, outputPath);
-
     const transcription = await openai.audio.transcriptions.create({
-      model: "whisper-1",
-      file: fs.createReadStream(outputPath),
+      model: 'whisper-1',
+      file: fs.createReadStream(filePath),
     });
 
     res.json({ transcription: transcription.text });
   } catch (error) {
     console.error(`Error on transcription! `, error);
-    res.status(500).json({ error: "Error on transcription!" });
+    res.status(500).json({ error: 'Error on transcription!' });
   } finally {
-    deleteFile(inputPath);
-    deleteFile(outputPath);
+    deleteFile(filePath);
   }
 });
 
